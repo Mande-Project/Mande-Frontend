e2b151bb6174610ba1ec621f4753361584ccda0f
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-10-02 20:55:47 -0500

    Functional logout button added to the header

--- client/api/index.js
+++ client/api/index.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 
+const token = typeof localStorage !== 'undefined' ? localStorage.getItem('token') : '';
+
 export const accessApi = axios.create({
-  baseURL: 'http://localhost:8000/api_users/'
-})
+  baseURL: 'http://localhost:8000/api_users/',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token ' + token,
+  },
+});
 
 
-
+export const baseApi = axios.create({
+  baseURL: 'http://localhost:8000/',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token ' + token,
+  },
+});
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,41 @@
+import { logoutUser } from '@/api/access';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+  const router = useRouter();
   const { name } = { name: 'Test Name' };
 
+  const logout = async () => {
+    const id = toast.loading('Loading...');
+    try {
+      const {data} = await logoutUser();
+      console.log(res);
+      showToast('promiseS', `${data}`, id);
+      setTimeout(() => {
+        localStorage.removeItem('token');
+        router.push('/login');
+      }, 2000);
+    } catch (error) {
+      const res = error.response;
+      console.log(res)
+      // showToast('promiseE', `${data}`, id);
+    }
+    router.push('/login');
+  };
+
   return (
-    <div className='flex justify-end mb-6'>
-      <p className='mr-2'>Hola: {name}</p>
+    <div className='sm:flex sm:justify-between mb-6'>
+      <p className='mr-2 mb-5 lg:mb-0'>Hi {name}</p>
+
+      <button
+        onClick={() => logout()}
+        type='button'
+        className='bg-blue-800 w-full sm:w-auto font-bold uppercase text-xs rounded py-1 px-2 text-white shadow-md'
+      >
+        Logout
+      </button>
     </div>
   );
 };
