fa537ba089bdebdad6558357eb911a5b5be89b2b
AlejoZA <alejandro.zamorano@correounivalle.edu.co>
2023-10-18 23:10:45 -0500

    Page

---
+++ Mande-Frontend/client/components/Toast.jsx
@@ -0,0 +1,88 @@
+import { toast } from 'react-toastify';
+
+const showToast = (type, message, idPromise, position) => {
+  switch (type) {
+    case 'warning':
+      toast.warn(message, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      break;
+    case 'info':
+      toast.info(message, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      break;
+    case 'success':
+      toast.success(message, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      break;
+    case 'error':
+      toast.error(message, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      break;
+    case 'promiseS':
+      toast.update(idPromise, {
+        render: `${message}`,
+        type: 'success',
+        isLoading: false,
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      break;
+
+    case 'promiseE':
+      toast.update(idPromise, {
+        render: `${message}`,
+        type: 'error',
+        isLoading: false,
+        // position: 'top-right',
+        position: position || 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      break;
+  }
+};
+
+export default showToast;
