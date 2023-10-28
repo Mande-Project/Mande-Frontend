a185646246f32b110457fbfa9898e06c7328f464
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-10-28 18:35:50 -0500

    The Layout and PrivateLayout logic was modified and the landing page was put in the index

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,8 @@
 import '@radix-ui/themes/styles.css';
 import Head from "next/head";
 import React from 'react';
-import Layout from "../components/Layout";
 import '../css/spinner.css';
 import './index.css';
-
 // eslint-disable-next-line react/prop-types
 const MyApp = ({ Component, pageProps }) => {
   return (
@@ -12,9 +10,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <title>MANDE</title>
       </Head>
-      <Layout>
         <Component {...pageProps} />
-      </Layout>
     </>
   )
 }
